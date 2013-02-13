83c042a90f8d5045113992ff78122566bdf193af
Jakub Kulig <info@jkulig.com>
2013-02-13 14:25:18 +0100

    First commit, create base controllers

---
+++ application/views/_layout_main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $meta_title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="<?php echo site_url('assets/css/bootstrap.css'); ?>" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="<?php echo site_url('assets/js/bootstrap.min.js'); ?>"></script>
+  </body>
+</html>
