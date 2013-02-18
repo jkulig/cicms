9bd6bfa143fe29e17389ed5bafc99e6676cd156a
Jakub Kulig <info@jkulig.com>
2013-02-18 14:45:25 +0100

    add authentication for admin pages

--- application/migrations/003_create_sessions.php
+++ application/migrations/003_create_sessions.php
@@ -12,14 +12,14 @@ class Migration_Create_Sessions extends CI_Migration {
 			'user_data text NOT NULL'
 		);
 		
-		$this->dbforge->add_fields($fields);
+		$this->dbforge->add_field($fields);
 		$this->dbforge->add_key('session_id', TRUE);
 		$this->dbforge->create_table('ci_sessions');
 		$this->db->query('ALTER TABLE `ci_sessions` ADD KEY `last_activity_idx` (`last_activity`)');
 	}
 	
 	public function down() {
-		$this->dbforge(drop_table)('ci_sessions');
+		$this->dbforge->drop_table('ci_sessions');
 	}
 	
 }
